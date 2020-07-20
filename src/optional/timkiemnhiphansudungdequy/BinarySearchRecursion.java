41a3400179f750791dfd428bc7a83168cc1aa4f8
huy8895 <huy8895@gmail.com>
2020-07-20 10:34:26 +0700

    13.5	[Optional] [Bài tập] Cài đặt thuật toán tìm kiếm nhị phân sử dụng đệ quy

---
+++ src/optional/timkiemnhiphansudungdequy/BinarySearchRecursion.java
@@ -0,0 +1,19 @@
+package optional.timkiemnhiphansudungdequy;
+
+public class BinarySearchRecursion {
+
+    int binarySearch(int[] array, int low, int high, int x) {
+        if (high >= low) {
+            int mid = low + (high - low) / 2;
+
+            if (array[mid] == x)
+                return mid;
+            else if (array[mid] > x)
+                return binarySearch(array, low, mid - 1, x);
+            else
+                return binarySearch(array, mid + 1, high, x);
+        }
+        return -1;
+    }
+}
+
