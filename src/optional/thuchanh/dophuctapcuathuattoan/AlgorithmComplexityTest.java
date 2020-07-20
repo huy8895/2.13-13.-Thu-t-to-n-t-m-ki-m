287dea07be013c163d1ebf1a0299aeaa3aee203a
huy8895 <huy8895@gmail.com>
2020-07-20 10:14:21 +0700

    [Optional] [Thực hành] Tính độ phức tạp của thuật toán

--- src/optional/thuchanh/dophuctapcuathuattoan/AlgorithmComplexityTest.java
+++ src/optional/thuchanh/dophuctapcuathuattoan/AlgorithmComplexityTest.java
@@ -1,22 +1,20 @@
+package optional.thuchanh.dophuctapcuathuattoan;
+
 import java.util.Scanner;
 
 public class AlgorithmComplexityTest {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
         System.out.println("Enter a string:");
-//        String inputString = scanner.nextLine();
         String inputString = "trinh van huy";
 
         int[] frequentChar = new int[255];
         for (int i = 0; i < inputString.length(); i++) {
-            /* Tìm ví trí tương ứng của ký tự trong bảng ASCII */
             int ascii = (int) inputString.charAt(i);
-            /* Tăng giá trị tần suất */
             frequentChar[ascii] += 1;
         }
 
         int max = 0;
-        char character = (char) 255; /* empty character */
+        char character = (char) 255;
         for (int j = 0; j < 255; j++) {
             if (frequentChar[j] > max) {
                 max = frequentChar[j];
